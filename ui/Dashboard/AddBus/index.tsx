081eaa98a711bec6791a0bd1587f633092e0a916
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-08-13 21:32:15 +0600

    complete add bus page

--- ui/Dashboard/AddBus/index.tsx
+++ ui/Dashboard/AddBus/index.tsx
@@ -1,10 +1,11 @@
 import { addBusStyles, userProfileStyles } from "@/lib/styles";
-import { Box, Button, Card, Select, Text } from "@mantine/core";
+import { Box, Button, Card, Select, Text, UnstyledButton } from "@mantine/core";
 import React from "react";
 import AddBusLogo from "@/public/add-buss.gif";
 import Image from "next/image";
 import Input from "@/ui/Input";
 import { IconChevronDown } from "@tabler/icons-react";
+import { domain } from "@/lib/api";
 const AddBuss = () => {
   const [busNumber, setBusNumber] = React.useState<string>("");
   const [totalSeats, setTotalSeats] = React.useState<string>("");
@@ -12,6 +13,32 @@ const AddBuss = () => {
   const { classes: input } = userProfileStyles({ Icon: null });
 
   const { classes } = addBusStyles();
+  const handleSaveBus = () => {
+    const data = {};
+    try {
+      fetch(`${domain}/api/buses`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => {
+          return res.json().then((responseData) => {
+            if (!res.ok) {
+              throw new Error(responseData.error || "An error occurred");
+            }
+            return responseData;
+          });
+        })
+        .then((data) => {
+          console.log("data");
+        })
+        .catch((error: any) => {
+          console.log("error", error.message);
+        });
+    } catch (error) {}
+  };
   return (
     <Box className={classes.container}>
       <Text>Add Bus</Text>
@@ -31,16 +58,42 @@ const AddBuss = () => {
           />
           <Input label="Seat Price" type="text" setState={setTotalSeats} />
           <Input label="Departure Time" type="text" setState={setTotalSeats} />
-          {/* <Input
-            isSelect
-            selectData={["AC", "Non-AC", "Coach-Bus"]}
-            label="Seat Price"
-            type="text"
-            setState={setTotalSeats}
-          /> */}
         </Box>
-        <Box style={{ alignItems: "flex-end" }}>
-          <Button>Save</Button>
+        <Box
+          sx={(theme) => ({
+            marginTop: 15,
+            [theme.fn.largerThan("sm")]: {
+              display: "flex",
+              // alignItems: "flex-end",
+              justifyContent: "flex-end",
+            },
+          })}
+        >
+          <Button
+            onClick={handleSaveBus}
+            sx={(theme) => ({
+              width: "100%",
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.gray[8]
+                  : theme.colors.gray[5],
+              textAlign: "center",
+              padding: "7px 3px",
+              [theme.fn.largerThan("sm")]: {
+                width: "10%",
+                padding: "10px 3px",
+              },
+              borderRadius: "5px",
+              "&:hover": {
+                backgroundColor: "inherit",
+                border: `1px solid ${
+                  theme.colorScheme === "dark" ? "#51D784" : ""
+                }`,
+              },
+            })}
+          >
+            Save
+          </Button>
         </Box>
       </Card>
     </Box>
